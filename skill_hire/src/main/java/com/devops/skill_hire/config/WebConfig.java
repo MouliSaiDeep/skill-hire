018890a1b4c8ece35ec6ce6c781df6acf0a9a040
Nithin <attilinithin@gmail.com>
2026-04-08 23:31:03 +0530

    added webconfig file

--- skill_hire/src/main/java/com/devops/skill_hire/config/WebConfig.java
+++ skill_hire/src/main/java/com/devops/skill_hire/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all API endpoints
                 // Add your exact OpenShift frontend route here
-                .allowedOrigins("frontend-route-23mh1a05l8-dev.apps.rm3.7wse.p1.openshiftapps.com") 
+                .allowedOrigins("https://frontend-route-23mh1a05l8-dev.apps.rm3.7wse.p1.openshiftapps.com") 
                 // Alternatively, use "*" for testing to allow all origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
